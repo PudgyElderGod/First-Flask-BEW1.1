c6d14f594882ce3ea303bbb5f3497c71083a0c0a
PudgyElderGod <PudgyElderGod@gmail.com>
2019-09-05 10:47:03 -0700

    New code

--- app.py
+++ app.py
@@ -1,40 +1,54 @@
 #Imports
-from flask import Flask, request
-from random import choice
+from flask import Flask, request, render_template
+from random import choice, sample
 
-app = Flask(__name__) #Don't do these yet
+app = Flask(__name__)
 
 #Lists
-compliments = ['coolio', 'smashing', 'neato', 'fantabulous']
+compliments = ['coolio', 'smashing', 'neato', 'fantabulous', 'radicool']
 horoscopes = ['Aries: Polish them horns.', 'Pisces: Do not eat seafood, you cannibal.', 'Sagitarius: Practice your archery. The olympics are coming.', 'Capricorn: stop being a big nerd.']
 
 
 #Functions
- #Don't touch this entire line block until the end of the class.
 @app.route('/compliment')
 def get_compliment():
     """Give the user a compliment"""
     name = request.args.get('name')
-    compliment = choice(compliments)
-    return f'Hello there, {name}! You are so {compliment}!'
+    show_compliments = request.args.get('show_compliments')
+    compliments_to_show = sample(compliments, 3)
+
+    return render_template(
+        'compliments.html',
+        name=name,
+        show_compliments=show_compliments,
+        compliments=compliments_to_show)
+# @app.route('/compliment')
+# def get_compliment():
+#     """Give the user a compliment"""
+#     name = request.args.get('name')
+#     show_compliments = request.args.get('show_compliments')
+#     compliment = choice(compliments)
+#     compliments_to_show = sample(compliments, 3)
+#
+#     return render_template(
+#         'compliments.html',
+#         name=name,
+#         show_compliments=show_compliments,
+#         compliment=compliment),
+#         compliments=compliments_to_show
+
 
 @app.route('/')
 def index():
     """Show the homepage and ask the user's name."""
-    return """
-    <form action='/compliment'>
-        What is your name?
-        <input type="text" name="name"></input>
-        <button type="submit">Submit</button>
-    </form>
-    """
-#Fuck the above
+    section = 'BEW 1.1 Section A'
+    greeting = 'Good Morning'
+    return render_template('index.html',
+    section = section,
+    greeting = greeting
+    )
 
-# Use this block for most of class.
-# @app.route('/compliment')
-# def get_compliment():
-#     compliment = choice(compliments)
-#     return f'Hello there, user! You are so {compliment}!'
+
 
 @app.route('/horoscope')
 def get_horoscope():
@@ -43,5 +57,5 @@ def get_horoscope():
 
 
 
-if __name__ == "__main__": #Don't do these yet.
+if __name__ == "__main__":
    app.run(debug=True)
